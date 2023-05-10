190d2e57d3f1629565d12236bd87af5cb232ae95
JRCRT <jericorito1712@gmail.com>
2023-05-10 13:55:04 +0800

    Added: Db Context

--- jts_backend/Models/JtsContext.cs
+++ jts_backend/Models/JtsContext.cs
@@ -2,13 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace jts_backend.Models
 {
     public class JtsContext : DbContext
     {
-        public JtsContext(DbContextOptions options) : base(options) { }
-        DbSet<UserModel> user
+        public JtsContext(DbContextOptions<JtsContext> options) : base(options) { }
+        public DbSet<UserModel> user
         {
             get;
             set;
--- jts_backend/Program.cs
+++ jts_backend/Program.cs
@@ -1,9 +1,14 @@
+using jts_backend.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<JtsContext>(opt =>
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DatabaseConnection")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -17,7 +22,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors(x => x.AllowAnyHeader()
+      .AllowAnyMethod()
+      .WithOrigins("http://localhost:5173"));
 app.UseAuthorization();
 
 app.MapControllers();
