1129bb24cca757ecc227b958e72a6a51c181a54c
JRCRT <jericorito1712@gmail.com>
2023-07-25 13:26:49 +0800

    Update

--- jts_backend/Hub/IJtsHub.cs
+++ jts_backend/Hub/IJtsHub.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 using jts_backend.Dtos.UserDto;
 
 namespace jts_backend.Hub
 {
     public interface IJtsHub
-    {
-         Task GetUser(GetUserDto user);
+    {   
+        Task GetUser(GetUserDto user);
+         
     }
 }
