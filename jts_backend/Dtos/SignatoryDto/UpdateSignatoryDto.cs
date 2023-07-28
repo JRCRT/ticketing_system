1ec59bb264db14c98834374e1fe33ece9544814c
JRCRT <jericorito1712@gmail.com>
2023-07-28 09:25:00 +0800

    Update

--- jts_backend/Dtos/SignatoryDto/UpdateSignatoryDto.cs
+++ jts_backend/Dtos/SignatoryDto/UpdateSignatoryDto.cs
@@ -8,9 +8,6 @@ namespace jts_backend.Dtos.SignatoryDto
     public class UpdateSignatoryDto
     {
         public int signatory_id {get;set;}
-        public int user_id { get; set; }
-        public string type { get; set; } = string.Empty;
-        public int ticket_id {get;set;}
         public int status_id{get;set;} 
     }
 }
