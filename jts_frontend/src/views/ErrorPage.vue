d60dba286651557a98af4cd24a0914032b92d6da
JRCRT <jericorito1712@gmail.com>
2023-08-11 10:27:40 +0800

    Update

--- jts_frontend/src/views/ErrorPage.vue
+++ jts_frontend/src/views/ErrorPage.vue
@@ -1,9 +1,541 @@
 <template>
-  <div>
-    <p>500</p>
-    <p>Internal Server Error.</p>
-    <p>Kindly contact your system administrator.</p>
-  </div>
+  <figure class="table" style="width: 816px">
+    <table class="ck-table-resized" style="border: 2px solid hsl(0, 0%, 0%)">
+      <colgroup>
+        <col style="width: 10.07%" />
+        <col style="width: 5.61%" />
+        <col style="width: 5.27%" />
+        <col style="width: 5.27%" />
+        <col style="width: 5.27%" />
+        <col style="width: 4.57%" />
+        <col style="width: 16.5%" />
+        <col style="width: 5.27%" />
+        <col style="width: 5.27%" />
+        <col style="width: 5.27%" />
+        <col style="width: 5.27%" />
+        <col style="width: 5.27%" />
+        <col style="width: 5.27%" />
+        <col style="width: 5.27%" />
+        <col style="width: 5.27%" />
+        <col style="width: 5.28%" />
+      </colgroup>
+      <tbody>
+        <tr>
+          <td
+            style="
+              border: 1px solid hsl(0, 0%, 0%);
+              height: 18.5pt;
+              width: 96pt;
+            "
+            colspan="2"
+          >
+            Approved Date
+          </td>
+          <td
+            style="border: 1px solid hsl(0, 0%, 0%); width: 192pt"
+            colspan="4"
+          >
+            &nbsp;
+          </td>
+          <td
+            style="
+              border: 1px solid hsl(0, 0%, 0%);
+              width: 288pt;
+              text-align: center;
+              vertical-align: top;
+            "
+            colspan="6"
+            rowspan="3"
+          >
+            APPLICATION (稟議書)
+          </td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            Applied Date
+          </td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            &nbsp;
+          </td>
+        </tr>
+        <tr>
+          <td
+            style="border: 1px solid hsl(0, 0%, 0%); height: 30pt; width: 96pt"
+            colspan="2"
+            rowspan="2"
+          >
+            Approved No.
+          </td>
+          <td
+            style="border: 1px solid hsl(0, 0%, 0%); width: 192pt"
+            colspan="4"
+            rowspan="2"
+          >
+            &nbsp;
+          </td>
+
+          <td
+            style="border: 1px solid hsl(0, 0%, 0%); width: 96pt"
+            colspan="2"
+            rowspan="2"
+          >
+            <p>Receipt No.</p>
+            <p>Receipt Date</p>
+          </td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            &nbsp;
+          </td>
+        </tr>
+        <tr>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            &nbsp;
+          </td>
+        </tr>
+
+        <tr>
+          <td
+            style="
+              border: 1px solid hsl(0, 0%, 0%);
+              height: 14.5pt;
+              vertical-align: top;
+              width: 48pt;
+            "
+            rowspan="4"
+          >
+            <p>決裁者</p>
+            <p>Approved by</p>
+          </td>
+          <td
+            style="
+              border: 1px solid hsl(0, 0%, 0%);
+              text-align: center;
+              width: 240pt;
+            "
+            colspan="5"
+          >
+            <p>JACCS CO., LTD</p>
+            <p>(BOARD OF DIRECTORS)&nbsp;</p>
+          </td>
+          <td
+            style="
+              border: 1px solid hsl(0, 0%, 0%);
+              text-align: center;
+              width: 288pt;
+            "
+            colspan="6"
+          >
+            <p>JACCS CO., LTD&nbsp;</p>
+            <p>(INTERNATIONAL BUSINESS DIVISION)</p>
+          </td>
+          <td
+            style="
+              background-color: hsl(180, 75%, 60%);
+              border: 1px solid hsl(0, 0%, 0%);
+              text-align: center;
+              width: 192pt;
+            "
+            colspan="4"
+          >
+            <p>JFP</p>
+            <p>(JACCS FINANCE PHILIPPINES CORPORATION)</p>
+          </td>
+        </tr>
+        <tr>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            □CEO
+          </td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 48pt">□COO</td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            □担当役員
+          </td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            □部長
+          </td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            課長
+          </td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 48pt">検印</td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 48pt">担当</td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            □President
+          </td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 48pt">EVP</td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 48pt">SVP</td>
+        </tr>
+        <tr>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            &nbsp;
+          </td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 48pt">&nbsp;</td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            &nbsp;
+          </td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            &nbsp;
+          </td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            &nbsp;
+          </td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 48pt">&nbsp;</td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 48pt">&nbsp;</td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            &nbsp;
+          </td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 48pt">&nbsp;</td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 48pt">&nbsp;</td>
+        </tr>
+        <tr>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            &nbsp;
+          </td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 48pt">&nbsp;</td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            &nbsp;
+          </td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            &nbsp;
+          </td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            &nbsp;
+          </td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 48pt">&nbsp;</td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 48pt">&nbsp;</td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            &nbsp;
+          </td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 48pt">&nbsp;</td>
+          <td style="width: 48pt">&nbsp;</td>
+        </tr>
+        <tr>
+          <td
+            style="
+              border: 1px solid hsl(0, 0%, 0%);
+              height: 14.5pt;
+              width: 48pt;
+            "
+          >
+            <p>件名</p>
+            <p>Subject</p>
+          </td>
+          <td
+            style="border: 1px solid hsl(0, 0%, 0%); width: 384pt"
+            colspan="8"
+          >
+            New Employee System Access
+          </td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            <p>Conditions&nbsp;</p>
+            <p>(承認条件)</p>
+          </td>
+          <td
+            style="border: 1px solid hsl(0, 0%, 0%); width: 240pt"
+            colspan="5"
+          >
+            &nbsp;
+          </td>
+        </tr>
+        <tr>
+          <td
+            style="
+              border: 1px solid hsl(0, 0%, 0%);
+              height: 14.5pt;
+              width: 48pt;
+            "
+            rowspan="6"
+          >
+            <p>事後回覧</p>
+            <p>Related Partys</p>
+          </td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            Party
+          </td>
+          <td
+            style="border: 1px solid hsl(0, 0%, 0%); width: 96pt"
+            colspan="2"
+            rowspan="2"
+          >
+            &nbsp;
+          </td>
+          <td
+            style="border: 1px solid hsl(0, 0%, 0%); width: 96pt"
+            colspan="2"
+            rowspan="2"
+          >
+            &nbsp;
+          </td>
+          <td
+            style="border: 1px solid hsl(0, 0%, 0%); width: 96pt"
+            colspan="2"
+            rowspan="2"
+          >
+            &nbsp;
+          </td>
+          <td
+            style="border: 1px solid hsl(0, 0%, 0%); width: 96pt"
+            colspan="2"
+            rowspan="2"
+          >
+            &nbsp;
+          </td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            Checked by&nbsp;
+          </td>
+          <td
+            style="border: 1px solid hsl(0, 0%, 0%); width: 144pt"
+            colspan="3"
+            rowspan="2"
+          >
+            &nbsp;
+          </td>
+        </tr>
+        <tr>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            (関係先)
+          </td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            (検印)
+          </td>
+        </tr>
+        <tr>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            Date&nbsp;
+          </td>
+          <td
+            style="border: 1px solid hsl(0, 0%, 0%); width: 96pt"
+            colspan="2"
+            rowspan="2"
+          >
+            &nbsp;
+          </td>
+          <td
+            style="border: 1px solid hsl(0, 0%, 0%); width: 96pt"
+            colspan="2"
+            rowspan="2"
+          >
+            &nbsp;
+          </td>
+          <td
+            style="border: 1px solid hsl(0, 0%, 0%); width: 96pt"
+            colspan="2"
+            rowspan="2"
+          >
+            &nbsp;
+          </td>
+          <td
+            style="border: 1px solid hsl(0, 0%, 0%); width: 96pt"
+            colspan="2"
+            rowspan="2"
+          >
+            &nbsp;
+          </td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            Prepared by&nbsp;
+          </td>
+          <td
+            style="border: 1px solid hsl(0, 0%, 0%); width: 144pt"
+            colspan="3"
+            rowspan="2"
+          >
+            &nbsp;
+          </td>
+        </tr>
+        <tr>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            (日付)
+          </td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            (作成者)
+          </td>
+        </tr>
+        <tr>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            Seal
+          </td>
+          <td
+            style="border: 1px solid hsl(0, 0%, 0%); width: 96pt"
+            colspan="2"
+            rowspan="2"
+          >
+            &nbsp;
+          </td>
+          <td
+            style="border: 1px solid hsl(0, 0%, 0%); width: 96pt"
+            colspan="2"
+            rowspan="2"
+          >
+            &nbsp;
+          </td>
+          <td
+            style="border: 1px solid hsl(0, 0%, 0%); width: 96pt"
+            colspan="2"
+            rowspan="2"
+          >
+            &nbsp;
+          </td>
+          <td
+            style="border: 1px solid hsl(0, 0%, 0%); width: 96pt"
+            colspan="2"
+            rowspan="2"
+          >
+            &nbsp;
+          </td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            Division&nbsp;
+          </td>
+          <td
+            style="border: 1px solid hsl(0, 0%, 0%); width: 144pt"
+            colspan="3"
+            rowspan="2"
+          >
+            &nbsp;
+          </td>
+        </tr>
+        <tr>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            (印)
+          </td>
+          <td style="border: 1px solid hsl(0, 0%, 0%); width: 96pt" colspan="2">
+            (作成部署)
+          </td>
+        </tr>
+        <tr>
+          <td
+            style="
+              border: 1px solid hsl(0, 0%, 0%);
+              height: 14.5pt;
+              width: 768pt;
+            "
+            colspan="16"
+          >
+            &nbsp;
+          </td>
+        </tr>
+        <tr>
+          <td
+            style="
+              background-color: hsl(180, 75%, 60%);
+              border: 1px solid hsl(0, 0%, 0%);
+              height: 14.5pt;
+              width: 768pt;
+            "
+            colspan="16"
+          >
+            1.&nbsp;&nbsp;&nbsp;&nbsp; BACKGROUND (背景)
+          </td>
+        </tr>
+        <tr>
+          <td
+            style="
+              border: 1px solid hsl(0, 0%, 0%);
+              height: 116pt;
+              width: 768pt;
+            "
+            colspan="16"
+          >
+            &nbsp;
+          </td>
+        </tr>
+        <tr>
+          <td
+            style="
+              background-color: hsl(180, 75%, 60%);
+              border: 1px solid hsl(0, 0%, 0%);
+              height: 14.5pt;
+              width: 768pt;
+            "
+            colspan="16"
+          >
+            2.&nbsp;&nbsp;&nbsp;&nbsp; CONTENTS (稟議内容)
+          </td>
+        </tr>
+        <tr>
+          <td
+            style="
+              border: 1px solid hsl(0, 0%, 0%);
+              height: 116pt;
+              width: 768pt;
+            "
+            colspan="16"
+          >
+            &nbsp;
+          </td>
+        </tr>
+        <tr>
+          <td
+            style="
+              background-color: hsl(180, 75%, 60%);
+              border: 1px solid hsl(0, 0%, 0%);
+              height: 14.5pt;
+              width: 768pt;
+            "
+            colspan="16"
+          >
+            3.&nbsp;&nbsp;&nbsp;&nbsp; REASONS (稟議理由)
+          </td>
+        </tr>
+        <tr>
+          <td
+            style="
+              border: 1px solid hsl(0, 0%, 0%);
+              height: 101.5pt;
+              width: 768pt;
+            "
+            colspan="16"
+          >
+            &nbsp;
+          </td>
+        </tr>
+        <tr>
+          <td
+            style="
+              background-color: hsl(180, 75%, 60%);
+              border: 1px solid hsl(0, 0%, 0%);
+              height: 14.5pt;
+              width: 768pt;
+            "
+            colspan="16"
+          >
+            4.&nbsp;&nbsp;&nbsp;&nbsp; OTHERS (その他)
+          </td>
+        </tr>
+        <tr>
+          <td
+            style="
+              border: 1px solid hsl(0, 0%, 0%);
+              height: 101.5pt;
+              width: 768pt;
+            "
+            colspan="16"
+          >
+            &nbsp;
+          </td>
+        </tr>
+        <tr>
+          <td
+            style="
+              background-color: hsl(180, 75%, 60%);
+              border: 1px solid hsl(0, 0%, 0%);
+              height: 14.5pt;
+              width: 768pt;
+            "
+            colspan="16"
+          >
+            5.&nbsp;&nbsp;&nbsp;&nbsp; ATTACHED DOCUMENTS (添付資料)
+          </td>
+        </tr>
+        <tr>
+          <td
+            style="
+              border: 1px solid hsl(0, 0%, 0%);
+              height: 101.5pt;
+              width: 768pt;
+            "
+            colspan="16"
+          >
+            &nbsp;
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </figure>
 </template>
 
 <script>
